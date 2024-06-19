a772dbbe2c4d13731b9a2a9514c4f1205768c348
Random-NPC <korrafan72@gmail.com>
2024-06-19 19:54:32 +0300

    test2\

--- script.js
+++ script.js
@@ -35,90 +35,141 @@ let six = document.querySelector("#six");
 let seven = document.querySelector("#seven");
 let eight = document.querySelector("#eight");
 let nine = document.querySelector("#nine");
-let equal = document.querySelector(".equal");
+const equal = document.querySelector(".equal");
 const decimal = document.querySelector("#decimal");
 let arrayOfNumbers = [0];
 let arrayOfNumbers2 = [0];
 
 function addNum(num) {
-  if (arrayOfNumbers[0] === 0 || arrayOfNumbers2[0] === 0) {
+  if (arrayOfNumbers[0] === 0) {
     arrayOfNumbers.pop();
-    arrayOfNumbers2.pop();
   }
   arrayOfNumbers.push(num);
   let fixedArray = arrayOfNumbers.join("");
   buffTxt.textContent = fixedArray;
 }
-
-zero.addEventListener("click", () => {
-  return addNum(0);
-});
-one.addEventListener("click", () => {
-  return addNum(1);
-});
-two.addEventListener("click", () => {
-  return addNum(2);
-});
-three.addEventListener("click", () => {
-  return addNum(3);
-});
-four.addEventListener("click", () => {
-  return addNum(4);
-});
-five.addEventListener("click", () => {
-  return addNum(5);
-});
-six.addEventListener("click", () => {
-  return addNum(6);
-});
-seven.addEventListener("click", () => {
-  return addNum(7);
-});
-eight.addEventListener("click", () => {
-  return addNum(8);
-});
-nine.addEventListener("click", () => {
-  return addNum(9);
-});
+function eventListeners1() {
+  zero.addEventListener("click", () => {
+    return addNum(0);
+  });
+  one.addEventListener("click", () => {
+    return addNum(1);
+  });
+  two.addEventListener("click", () => {
+    return addNum(2);
+  });
+  three.addEventListener("click", () => {
+    return addNum(3);
+  });
+  four.addEventListener("click", () => {
+    return addNum(4);
+  });
+  five.addEventListener("click", () => {
+    return addNum(5);
+  });
+  six.addEventListener("click", () => {
+    return addNum(6);
+  });
+  seven.addEventListener("click", () => {
+    return addNum(7);
+  });
+  eight.addEventListener("click", () => {
+    return addNum(8);
+  });
+  nine.addEventListener("click", () => {
+    return addNum(9);
+  });
+}
+eventListeners1();
 // operators
 const addBtn = document.querySelector(".plus");
 const subtractBtn = document.querySelector(".subt");
 const multiplyBtn = document.querySelector(".times");
 const divideBtn = document.querySelector(".divi");
 let op = null;
+
 function getOperator(operator) {
   op = operator;
-  let num1 = arrayOfNumbers.join("");
   buffTxt.textContent = "";
 }
+function getSecondNum(num) {
+  if (arrayOfNumbers2[0] === 0) {
+    arrayOfNumbers2.pop();
+  }
+  arrayOfNumbers2.push(num);
+  let fixedArray2 = arrayOfNumbers2.join("");
+  buffTxt.textContent = fixedArray2;
+}
+function eventListeners2() {
+  zero.addEventListener("click", () => {
+    getSecondNum(0);
+  });
+  one.addEventListener("click", () => {
+    getSecondNum(1);
+  });
+  two.addEventListener("click", () => {
+    getSecondNum(2);
+  });
+  three.addEventListener("click", () => {
+    getSecondNum(3);
+  });
+  four.addEventListener("click", () => {
+    getSecondNum(4);
+  });
+  five.addEventListener("click", () => {
+    getSecondNum(5);
+  });
+  six.addEventListener("click", () => {
+    getSecondNum(6);
+  });
+  seven.addEventListener("click", () => {
+    getSecondNum(7);
+  });
+  eight.addEventListener("click", () => {
+    getSecondNum(8);
+  });
+  nine.addEventListener("click", () => {
+    getSecondNum(9);
+  });
+}
 addBtn.addEventListener("click", () => {
-  return getOperator("+");
+  return [getOperator("+"), eventListeners2()];
 });
 subtractBtn.addEventListener("click", () => {
-  return getOperator("-");
+  return [getOperator("-"), eventListeners2()];
 });
 multiplyBtn.addEventListener("click", () => {
-  return getOperator("*");
+  return [getOperator("*"), eventListeners2()];
 });
 divideBtn.addEventListener("click", () => {
-  return getOperator("/");
+  return [getOperator("/"), eventListeners2()];
 });
-
-function getSecondNum(num) {}
+equal.addEventListener("click", () => {
+  let num1 = arrayOfNumbers.join("");
+  console.log(num1);
+  let num2 = arrayOfNumbers2.join("");
+  console.log(num2);
+  let sum = operate(num1, op, num2);
+  console.log(sum);
+  buffTxt.textContent = sum;
+  eventListeners1();
+});
 buffer.appendChild(buffTxt);
-calculator.appendChild(buffer);
-calculator.appendChild(divideBtn);
-calculator.appendChild(seven);
-calculator.appendChild(eight);
-calculator.appendChild(nine);
-calculator.appendChild(multiplyBtn);
-calculator.appendChild(four);
-calculator.appendChild(five);
-calculator.appendChild(six);
-calculator.appendChild(subtractBtn);
-calculator.appendChild(one);
-calculator.appendChild(two);
-calculator.appendChild(three);
-calculator.appendChild(equal);
-calculator.appendChild(addBtn);
-calculator.appendChild(zero);
+calculator.append(
+  buffer,
+  divideBtn,
+  seven,
+  eight,
+  nine,
+  multiplyBtn,
+  four,
+  five,
+  six,
+  subtractBtn,
+  one,
+  two,
+  three,
+  equal,
+  addBtn,
+  zero,
+);
