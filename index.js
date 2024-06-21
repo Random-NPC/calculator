5b0b2403e8a25d4787dce0e809571b4b046feb87
Random-NPC <korrafan72@gmail.com>
2024-06-21 16:37:01 +0300

    Finished Project 3/3

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function operate(num1, op, num2) {
 }
 let num1 = "";
 let num2 = "";
-let operator = null;
+let operator = "";
 let sum = "";
 const buffTxt = document.querySelector("#buffer-txt");
 function addNum(num) {
@@ -34,48 +34,65 @@ function addNum(num) {
   } else if (sum !== "") {
     num1 += num;
     buffTxt.textContent = num1;
+  } else if (num1 === "" && sum !== "" && num2 === "") {
+    num2 += num;
+    buffTxt.textContent = num2;
   } else {
     num2 += num;
     buffTxt.textContent = num2;
   }
 }
 const zero = document.querySelector("#extend").addEventListener("click", () => {
-  return addNum(0);
+  return addNum(0), decimalCheck();
 });
 const one = document.querySelector("#one").addEventListener("click", () => {
-  return addNum(1);
+  return [addNum(1)];
 });
 const two = document.querySelector("#two").addEventListener("click", () => {
-  return addNum(2);
+  return [addNum(2)];
 });
 const three = document.querySelector("#three").addEventListener("click", () => {
-  return addNum(3);
+  return [addNum(3)];
 });
 const four = document.querySelector("#four").addEventListener("click", () => {
-  return addNum(4);
+  return [addNum(4)];
 });
 const five = document.querySelector("#five").addEventListener("click", () => {
-  return addNum(5);
+  return [addNum(5)];
 });
 const six = document.querySelector("#six").addEventListener("click", () => {
-  return addNum(6);
+  return [addNum(6)];
 });
 const seven = document.querySelector("#seven").addEventListener("click", () => {
-  return addNum(7);
+  return [addNum(7)];
 });
 const eight = document.querySelector("#eight").addEventListener("click", () => {
-  return addNum(8);
+  return [addNum(8)];
 });
 const nine = document.querySelector("#nine").addEventListener("click", () => {
-  return addNum(9);
+  return [addNum(9)];
 });
 function getOperator(op) {
   if (sum === "") {
-    operator = op;
+    operator += op;
     buffTxt.textContent = num2;
   } else if (sum !== "") {
-    operator = op;
+    operator += op;
     buffTxt.textContent = num1;
+  }
+  if (operator.length > 1) {
+    if (sum === "") {
+      let newNum1 = parseInt(num1);
+      let newNum2 = parseInt(num2);
+      sum = operate(newNum1, operator[0], newNum2);
+      num1 = "";
+      num2 = "";
+      buffTxt.textContent = sum;
+    } else if (sum !== "") {
+      let newNum1 = parseInt(num1);
+      sum = operate(sum, operator[operator.length - 2], newNum1);
+      buffTxt.textContent = sum;
+    }
   }
 }
 const addBtn = document.querySelector(".plus").addEventListener("click", () => {
@@ -95,17 +112,54 @@ const divBtn = document.querySelector(".divi").addEventListener("click", () => {
 
 const equal = document.querySelector(".equal").addEventListener("click", () => {
   if (sum === "") {
-    let newNum1 = parseInt(num1);
-    let newNum2 = parseInt(num2);
+    let newNum1 = parseFloat(num1);
+    let newNum2 = parseFloat(num2);
     num1 = "";
     num2 = "";
     sum = operate(newNum1, operator, newNum2);
+    Math.round(sum * 10) / 10;
     buffTxt.textContent = sum;
-  } else if (sum !== "") {
-    let newNum1 = parseInt(num1);
+    operator = "";
+  } else if (sum !== "" && operator.length === 1) {
+    let newNum1 = parseFloat(num1);
     num1 = "";
     sum = operate(newNum1, operator, sum);
+    Math.round(sum * 10) / 10;
     console.log(sum);
+    operator = "";
     buffTxt.textContent = sum;
+  } else if (operator.length > 1) {
+    let newNum1 = parseFloat(num1);
+    sum = operate(sum, operator[operator.length - 1], newNum1);
+    Math.round(sum * 10) / 10;
+    buffTxt.textContent = sum;
+  } else if (operator == "" || num2 == "" || sum == "" || num1 == "") {
+    buffTxt.textContent = "ERROR";
+    num1 = "";
+    num2 = "";
+    operator = "";
+    sum = "";
   }
 });
+
+const clear = document.querySelector("#clear").addEventListener("click", () => {
+  num1 = "";
+  num2 = "";
+  operator = "";
+  sum = "";
+  buffTxt.textContent = 0;
+});
+const backspace = document
+  .querySelector("#del")
+  .addEventListener("click", () => {
+    if (sum === "" && operator !== "") {
+      num2 = num2.slice(0, -1);
+      buffTxt.textContent = num2;
+    } else if (
+      (sum !== "" && operator !== "") ||
+      (sum === "" && operator === "")
+    ) {
+      num1 = num1.slice(0, -1);
+      buffTxt.textContent = num1;
+    }
+  });
